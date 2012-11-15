55ad0acf261306145350c04a971b2785b76c655c
Kevin Sweeney <kevin.t.sweeney@gmail.com>
2012-11-15 12:10:03 -0800

    Collection of perf data in separate run

--- scripts/benchmark-for-commit.sh
+++ scripts/benchmark-for-commit.sh
@@ -14,15 +14,39 @@ if [[ $# -lt 1 ]]; then
   exit 1
 fi
 
+# These are used by the perf-ruby.sh wrapper script for perf data
+export RUBY_PARGC_BENCHMARK_LOGS_DIR="$(dirname "$0")/../benchmark-logs"
+export RUBY_PARGC_COMMIT="$1"
+
 : "${GIT_REPO_ROOT:="$HOME/workspace/ruby"}"
 
 rsync -av --stats $HOME/workspace/ruby/ ruby-clone
+
 pushd ruby-clone
-git checkout "$1"
+git checkout "$RUBY_PARGC_COMMIT"
 make -j16 CC="ccache gcc" CFLAGS="-O2 -fPIC -g"
-mkdir -p ../benchmark-logs
+mkdir -p "$RUBY_PARGC_BENCHMARK_LOGS_DIR"
 
 ulimit -s 16384 # To prevent stack overflow
-ruby benchmark/driver.rb -e './ruby' -q -o ../benchmark-logs/ruby-bm-"$1".log -d benchmark -p 'bm_'
+
+echo "Benchmarking without perf (raw opaque benchmark)" >&2
+ruby benchmark/driver.rb \
+  -e './ruby' \
+  -q \
+  -o "$RUBY_PARGC_BENCHMARK_LOGS_DIR/ruby-bm-$RUBY_PARGC_COMMIT" \
+  -d benchmark \
+  -p 'bm_'
+
+echo "Benchmarking with perf wrapper" >&2
+# Opaque results would include bash runtime so they are discarded.
+# perf data is collected by perf-ruby.sh
+ruby benchmark/driver.rb \
+  -e '../scripts/perf-ruby.sh ./ruby' \
+  -q \
+  -o /dev/null \
+  -d benchmark \
+  -p 'bm_'
+
 popd
+
 rm -fr ruby-clone
